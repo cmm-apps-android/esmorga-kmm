6df2951c02f6b6c44747a77dca1cf1cb10814210
m.a.perez.fernandez <m.a.perez.fernandez@accenture.com>
2026-08-04 09:50:48 +0200

    Update base url for qa

--- datasource-remote/src/commonMain/kotlin/cmm/apps/datasource/remote/di/RemoteDIModule.kt
+++ datasource-remote/src/commonMain/kotlin/cmm/apps/datasource/remote/di/RemoteDIModule.kt
@@ -1,16 +1,13 @@
 package cmm.apps.datasource.remote.di
 
-import cmm.apps.datasource.remote.api.EsmorgaApi
-import cmm.apps.datasource.remote.event.EventRemoteDatasourceImpl
-import cmm.apps.datasource.remote.user.UserRemoteDatasourceImpl
 import cmm.apps.data.di.DataDIModule
 import cmm.apps.data.event.datasource.EventDatasource
 import cmm.apps.data.user.datasource.UserDatasource
+import cmm.apps.datasource.remote.api.EsmorgaApi
 import cmm.apps.datasource.remote.api.NetworkApiHelper
+import cmm.apps.datasource.remote.event.EventRemoteDatasourceImpl
+import cmm.apps.datasource.remote.user.UserRemoteDatasourceImpl
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -20,7 +17,7 @@ object RemoteDIModule {
         factory<EventDatasource>(named(DataDIModule.REMOTE_DATASOURCE_INSTANCE_NAME)) { EventRemoteDatasourceImpl(get()) }
         factory<UserDatasource>(named(DataDIModule.REMOTE_DATASOURCE_INSTANCE_NAME)) { UserRemoteDatasourceImpl(get()) }
         single<HttpClient> {
-            NetworkApiHelper().provideApi("https://qa.esmorga.canarte.org/v1/")
+            NetworkApiHelper().provideApi("https://qa.api.esmorgaevents.com/v1/")
         }
         single<EsmorgaApi> { EsmorgaApi(get()) }
     }
