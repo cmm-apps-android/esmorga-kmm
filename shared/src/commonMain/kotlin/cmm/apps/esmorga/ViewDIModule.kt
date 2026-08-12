f8d99917cbb40b1c5625a6db56e0c0f8921ed816
m.a.perez.fernandez <m.a.perez.fernandez@accenture.com>
2026-08-12 11:05:40 +0200

    Translate strings

--- shared/src/commonMain/kotlin/cmm/apps/esmorga/ViewDIModule.kt
+++ shared/src/commonMain/kotlin/cmm/apps/esmorga/ViewDIModule.kt
@@ -5,26 +5,19 @@ import cmm.apps.viewmodel.eventlist.EventListViewModel
 import cmm.apps.viewmodel.login.LoginViewModel
 import cmm.apps.viewmodel.registration.RegistrationViewModel
 import cmm.apps.viewmodel.welcome.WelcomeViewModel
-import org.koin.compose.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 object ViewDIModule {
 
     val modules = module {
-        viewModel {
-            EventListViewModel(get())
-        }
+        viewModelOf(::EventListViewModel)
         viewModel { (eventId: String) ->
             EventDetailsViewModel(get(), eventId)
         }
-        viewModel {
-            WelcomeViewModel(get())
-        }
-        viewModel {
-            LoginViewModel(get())
-        }
-        viewModel {
-            RegistrationViewModel(get())
-        }
+        viewModelOf(::WelcomeViewModel)
+        viewModelOf(::LoginViewModel)
+        viewModelOf(::RegistrationViewModel)
     }
 }
