d2d3aa9fd36a0c905214633e922a357e516307ce
m.a.perez.fernandez <m.a.perez.fernandez@accenture.com>
2026-09-17 11:47:55 +0200

    Rename explore view

--- shared/src/commonMain/kotlin/cmm/esmorga/screens/explore/ExploreView.kt
+++ shared/src/commonMain/kotlin/cmm/esmorga/screens/explore/ExploreView.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
